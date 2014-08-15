88a33a537cec56a139a3cdace94df93b38453d04
Britone Mwasaru <bmwasaru@gmail.com>
2014-08-15 14:13:22 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -6,3 +6,21 @@
 >>> print 'Hello World'
 Hello World
 
+>>> name = 'Britone Mwasaru' # save the string into a variable called name
+
+>>> print name # outputs the value of name
+Britone Mwasaru
+
+>>> len(name) # len() function get the length of the string
+15
+
+>>> name[0] # python uses zero indexing name[0] gets the first character in the string name
+'B'
+
+>>> name[-1] # gets the last character in the string name
+'u'
+
+# mathematical operations on strings
+>>> name[0]*3
+'BBB'
+
